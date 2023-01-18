ded3e363485203f0508f91f8057e9b604158a328
Chuck Kosman <chuck@ninetailed.com>
2023-01-18 09:46:01 -0800

    Modify Contentful plugin to fetch data in dev mode

--- marketing-contentful-nuxt/plugins/contentful.ts
+++ marketing-contentful-nuxt/plugins/contentful.ts
@@ -1,8 +1,18 @@
+import { defineNuxtPlugin } from "#app";
+
+// for dev
+import { createClient } from "contentful";
+
+// for SSR, SSG
 import contentful from "contentful";
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
-  const client = contentful.createClient({
+  const createClientFunc =
+    process.env.NODE_ENV === "development"
+      ? createClient
+      : contentful.createClient;
+  const client = createClientFunc({
     space: config.public.CONTENTFUL_SPACE_ID,
     accessToken: config.public.CONTENTFUL_TOKEN,
   });
